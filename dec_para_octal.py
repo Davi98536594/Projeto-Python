f2dc1aa3660f547440e9036c46e12b4c8e6cb569
Alex-031 <162382708+Alex-031@users.noreply.github.com>
2024-06-02 08:02:02 -0300

    dec_para_octal.py

--- dec_para_octal.py
+++ dec_para_octal.py
@@ -1,14 +1,14 @@
-#conversor de decimal para octal
-def decimal_tranformando_octal(numero):
-    if numero == 0:
-        return 0
+def dec_pra_octal(numero):
+    if numero < 0:
+        return "Número inválido"
+    elif numero == 0:
+        return "0"
     else:
-        resultado = ""
-        while numero != 0:
+        octal = ""
+        while numero > 0:
             resto = numero % 8
-            resultado = str(resto) + resultado
-            numero = numero // 8
-            return resultado
-numero = int(input("Digite um número intero decial para transformar em octal: "))
-octal = decimal_tranformando_octal(numero)
-print("{} convertido em octal é {}".format(numero, octal))
+            octal = str(resto) + octal
+            numero //= 8
+        return octal
+num_decimal = int(input("Digite um número decimal: "))
+print("O número octal correspondente é:", dec_pra_octal(num_decimal))
