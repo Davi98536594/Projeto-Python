1f189cea60afda3e5a6dea2719f583b8e7bcd8d7
Alex-031 <162382708+Alex-031@users.noreply.github.com>
2024-06-02 08:09:07 -0300

    binario_pra_hexa.py

--- binario_pra_hexa.py
+++ binario_pra_hexa.py
@@ -1,19 +1,7 @@
-def dec_pra_hexa(numero):
-    if numero < 0:
-        return "Número inválido"
-    elif numero == 0:
-        return "0"
-    else:
-        hexadecimal = ""
-        while numero > 0:
-            resto = numero % 16
-            if resto < 10:
-                hexadecimal = str(resto) + hexadecimal
-            else:
-                hexadecimal = chr(ord('A') + resto - 10) + hexadecimal
-            numero //= 16
-        return hexadecimal
+def binario_para_hexadecimal(num_bin):
+    numero_decimal = int(num_bin, 2)  
+    hexadecimal = hex(numero_decimal).upper()[2:]  
+    return hexadecimal
 
-# Exemplo de uso:
-num_dec = int(input("Digite um número decimal: "))
-print("O número hexadecimal correspondente é:", dec_pra_hexa(num_dec))
+num_bin = input("Digite um número binário: ")
+print("O número hexadecimal correspondente é:", binario_para_hexadecimal(num_bin))
