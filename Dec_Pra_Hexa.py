da42bae5a6761bb896a6363af6287eba17a3ea4e
Alex-031 <162382708+Alex-031@users.noreply.github.com>
2024-06-02 08:15:19 -0300

    Dec_Pra_Hexa.py

--- Dec_Pra_Hexa.py
+++ Dec_Pra_Hexa.py
@@ -1,8 +1,19 @@
-def decimal_to_hex(n):
-    hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
-    result = []
-    while n > 0:
-        result.append(hex[n % 16])
-        n = n // 16
-    result.reverse()
-    return ''.join(result)
+def dec_pra_hexa(numero):
+    if numero < 0:
+        return "Número inválido"
+    elif numero == 0:
+        return "0"
+    else:
+        hexadecimal = ""
+        while numero > 0:
+            resto = numero % 16
+            if resto < 10:
+                hexadecimal = str(resto) + hexadecimal
+            else:
+                hexadecimal = chr(ord('A') + resto - 10) + hexadecimal
+            numero //= 16
+        return hexadecimal
+
+# Exemplo de uso:
+num_dec = int(input("Digite um número decimal: "))
+print("O número hexadecimal correspondente é:", dec_pra_hexa(num_dec))
