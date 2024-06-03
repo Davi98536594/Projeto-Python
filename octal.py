1621659a2c5ba2bae2452d5cb075958401e29453
Breno Brandão <brenoow@proton.me>
2024-06-03 04:13:14 -0300

    aaaaa

--- octal.py
+++ octal.py
@@ -8,3 +8,16 @@ def octal_to_decimal(octal_str):
     decimal_fraction = sum(int(digit) * 8 ** -(index + 1) for index, digit in enumerate(fractional_part))
 
     return decimal_integer #+ decimal_fraction
+
+
+def hex_to_decimal(hex_str):
+        decimal = 0
+        hex_str = hex_str[::-1]  # Reverse the hex string
+        for i in range(len(hex_str)):
+            digit = hex_str[i]
+            if digit.isdigit():
+                decimal += int(digit) * (16 ** i)
+            else:
+                decimal += (ord(digit.upper()) - 55) * (16 ** i)
+
+        return decimal
